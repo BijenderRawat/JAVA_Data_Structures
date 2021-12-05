920dcdaa4499e91e7202008d6cd2a2f854498d18
Bijender Rawat <bijenderrawat12414@gmail.com>
2021-12-05 12:21:43 +0530

    An updated version of Kadanes algorithm

--- Kadanes_Algorithm.java
+++ Kadanes_Algorithm.java
@@ -5,27 +5,46 @@ public class Kadanes_Algorithm {
 	
 	public static void maxsubarray(int [] array) {
 		
-		int maxsum = 0, currentsum = 0;
-		
-		ArrayList<Integer> arraytoprint = new ArrayList<Integer>();
-		ArrayList<Integer> arraytoadd = new ArrayList<Integer>();
-		
+		int sum1 = 0, sum2 = 0;
+		Stack<Integer> s1 = new Stack<Integer>();
+		Stack<Integer> s2 = new Stack<Integer>();
 		for(int i = 0; i < array.length; i++) {
-			currentsum += array[i];
-			if(currentsum > maxsum) {
-				maxsum = currentsum;
-				arraytoprint.addAll(arraytoadd);
-				arraytoprint.add(array[i]);
+			if(!s2.isEmpty()) {
+				if(sum2 + array[i] > sum1) {
+					s1.addAll(s2);
+					s2.clear();
+					s1.push(array[i]);
+					sum1 = sum2 + array[i];
+					sum2 = 0;
+				}
+				else if(sum2 + array[i] == sum1){
+					s1.clear();
+					s1.push(array[i]);
+					s2.clear();
+					sum2 = 0;
+				}
+				else{
+					s2.push(array[i]);
+				}
 			}
-			else if(currentsum < 0) {
-				currentsum = 0;
-				arraytoprint.clear();
+			else if(sum1 + array[i] == 0) {
+				s1.clear();
+				sum1 = 0;
 			}
-			else arraytoadd.add(array[i]);
+			else if(sum1 + array[i] > sum1) {
+				s1.push(array[i]);
+				sum1 += array[i];
+			}else {
+				s2.push(array[i]);
+				sum2 = sum1 + array[i];
+			}
 		}
-		
+		int maxsum = 0;
 		System.out.println("Max SubArray");
-		for(int i : arraytoprint) System.out.print(i + " ");
+		for(int i : s1) {
+			System.out.print(i + " ");
+			maxsum += i;
+		}
 		System.out.println("\nMax Sum = " + maxsum);
 		
 	}
