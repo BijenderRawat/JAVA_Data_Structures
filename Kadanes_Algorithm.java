a32d4f6168b629f6dc5826d81b1363263357b0a4
Bijender Rawat <bijenderrawat12414@gmail.com>
2021-12-05 17:10:32 +0530

    A more updated version of kadanes algorithm that will print the max sum subarray also

--- Kadanes_Algorithm.java
+++ Kadanes_Algorithm.java
@@ -5,12 +5,17 @@ public class Kadanes_Algorithm {
 	
 	public static void maxsubarray(int [] array) {
 		
-		int sum1 = 0, sum2 = 0;
+		int sum1 = 0, sum2 = 0, temp = 0;
 		Stack<Integer> s1 = new Stack<Integer>();
 		Stack<Integer> s2 = new Stack<Integer>();
 		for(int i = 0; i < array.length; i++) {
 			if(!s2.isEmpty()) {
-				if(sum2 + array[i] > sum1) {
+				if(sum1 < array[i]) {
+					s1.clear();
+					s2.clear();
+					s1.push(array[i]);
+				}
+				else if(sum2 + array[i] > sum1) {
 					s1.addAll(s2);
 					s2.clear();
 					s1.push(array[i]);
@@ -23,7 +28,16 @@ public class Kadanes_Algorithm {
 					s2.clear();
 					sum2 = 0;
 				}
-				else s2.push(array[i]);
+				else {
+					if(array[i] >= 0 && temp != 0 && s2.peek() < 0) {
+						temp += array[i];
+						s2.push(array[i]);
+						sum2 += array[i];
+					}else if(array[i] < 0) {
+						s2.push(array[i]);
+						sum2 += array[i];
+					}
+				}
 			}
 			else if(sum1 + array[i] == 0) {
 				s1.clear();
